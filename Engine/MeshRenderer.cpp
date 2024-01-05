39ca3073913a97635a054ee2b3aaabda158efab2
hegria <awzs159@gmail.com>
2024-01-05 23:46:54 +0900

    week6

--- Engine/MeshRenderer.cpp
+++ Engine/MeshRenderer.cpp
@@ -4,6 +4,7 @@
 #include "Game.h"
 #include "Mesh.h"
 #include "Shader.h"
+#include "Material.h"
 
 MeshRenderer::MeshRenderer() : Super(ComponentType::MeshRenderer)
 {
@@ -15,21 +16,57 @@ MeshRenderer::~MeshRenderer()
 
 }
 
+//void MeshRenderer::Update()
+//{
+//	if (_mesh == nullptr || _texture == nullptr || _shader == nullptr)
+//		return;
+//
+//	_shader->GetSRV("Texture0")->SetResource(_texture->GetComPtr().Get());
+//
+//	auto world = GetTransform()->GetWorldMatrix();
+//	RENDER->PushTransformData(TransformDesc{world});
+//
+//	uint32 stride = _mesh->GetVertexBuffer()->GetStride();
+//	uint32 offset = _mesh->GetVertexBuffer()->GetOffset();
+//
+//	DC->IASetVertexBuffers(0, 1, _mesh->GetVertexBuffer()->GetComPtr().GetAddressOf(), &stride, &offset);
+//	DC->IASetIndexBuffer(_mesh->GetIndexBuffer()->GetComPtr().Get(), DXGI_FORMAT_R32_UINT, 0);
+//
+//	_shader->DrawIndexed(0, 0, _mesh->GetIndexBuffer()->GetCount(), 0, 0);
+//}
+
+//void MeshRenderer::Update()
+//{
+//	if (_mesh == nullptr || _texture == nullptr || _shader == nullptr)
+//		return;
+//
+//	_shader->GetSRV("DiffuseMap")->SetResource(_texture->GetComPtr().Get());
+//
+//	auto world = GetTransform()->GetWorldMatrix();
+//	RENDER->PushTransformData(TransformDesc{ world });
+//
+//	uint32 stride = _mesh->GetVertexBuffer()->GetStride();
+//	uint32 offset = _mesh->GetVertexBuffer()->GetOffset();
+//
+//	DC->IASetVertexBuffers(0, 1, _mesh->GetVertexBuffer()->GetComPtr().GetAddressOf(), &stride, &offset);
+//	DC->IASetIndexBuffer(_mesh->GetIndexBuffer()->GetComPtr().Get(), DXGI_FORMAT_R32_UINT, 0);
+//
+//	_shader->DrawIndexed(0, 0, _mesh->GetIndexBuffer()->GetCount(), 0, 0);
+//}
+
 void MeshRenderer::Update()
 {
-	if (_mesh == nullptr || _texture == nullptr || _shader == nullptr)
+	if (_mesh == nullptr || _material == nullptr)
 		return;
 
+	auto shader = _material->GetShader();
+	if (shader == nullptr)
+		return;
+
+	_material->Update();
+
 	auto world = GetTransform()->GetWorldMatrix();
-	_shader->GetMatrix("World")->SetMatrix((float*)&world);
-	
-	_shader->GetMatrix("View")->SetMatrix((float*)&Camera::S_MatView);
-	_shader->GetMatrix("Projection")->SetMatrix((float*)&Camera::S_MatProjection);
-	_shader->GetSRV("Texture0")->SetResource(_texture->GetComPtr().Get());
-	
-	// TEMP
-	Vec3 lightDir = {0.f, 0.f, 1.f};
-	_shader->GetVector("LightDir")->SetFloatVector((float*)&lightDir);
+	RENDER->PushTransformData(TransformDesc{ world });
 
 	uint32 stride = _mesh->GetVertexBuffer()->GetStride();
 	uint32 offset = _mesh->GetVertexBuffer()->GetOffset();
@@ -37,5 +74,5 @@ void MeshRenderer::Update()
 	DC->IASetVertexBuffers(0, 1, _mesh->GetVertexBuffer()->GetComPtr().GetAddressOf(), &stride, &offset);
 	DC->IASetIndexBuffer(_mesh->GetIndexBuffer()->GetComPtr().Get(), DXGI_FORMAT_R32_UINT, 0);
 
-	_shader->DrawIndexed(0, 0, _mesh->GetIndexBuffer()->GetCount(), 0, 0);
+	shader->DrawIndexed(0, 0, _mesh->GetIndexBuffer()->GetCount(), 0, 0);
 }
